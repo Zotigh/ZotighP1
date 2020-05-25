4246fe289477b93c01ef4eacf1fc34d1aaea58df
Lance Zotigh <lancezotighlz@gmail.com>
2020-05-24 20:14:23 -0600

    Add project files.

---
+++ ZotighP1/Program.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************
+* Programmer: Lance Zotigh (lzotigh@cnm.edu)
+* Program: IdealGasCal
+* Date: 2/24/2020
+* Purpose: To claculate the pressure exertied by a given gas
+* project.
+*******************************************************/
+
+using System;
+
+namespace ZotighIdealGasCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lance Zotigh");
+            Console.WriteLine("\nIdeal Gas Calc");
+            Console.WriteLine("\nObjective: To calculate the pressure of a given gas.");
+        }
+    }
+}
